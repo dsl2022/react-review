ef034c7f1cc4971a89aa22df21efdfdfd1bf4d7c
dsl2022 <2015rpro@gmail.com>
2023-08-10 22:23:58 -0400

    updated

--- src/components/SubmitForm.js
+++ src/components/SubmitForm.js
@@ -8,18 +8,10 @@ import Row from 'react-bootstrap/Row';
 function SubmitForm({names, setNames}) {
     const [firstName,setFirstName]=useState("")
     const [lastName,setLastName]=useState("")
-//   const [validated, setValidated] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault()
     setNames([...names,{firstName,lastName}])
-    // const form = event.currentTarget;
-    // if (form.checkValidity() === false) {
-    //   event.preventDefault();
-    //   event.stopPropagation();
-    // }
-
-    // setValidated(true);
   };
 
   function handleOnChangeFirst(e){
@@ -56,37 +48,6 @@ function SubmitForm({names, setNames}) {
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
       </Row>
-      {/* <Row className="mb-3">
-        <Form.Group as={Col} md="6" controlId="validationCustom03">
-          <Form.Label>City</Form.Label>
-          <Form.Control type="text" placeholder="City" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid city.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="3" controlId="validationCustom04">
-          <Form.Label>State</Form.Label>
-          <Form.Control type="text" placeholder="State" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid state.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="3" controlId="validationCustom05">
-          <Form.Label>Zip</Form.Label>
-          <Form.Control type="text" placeholder="Zip" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid zip.
-          </Form.Control.Feedback>
-        </Form.Group>
-      </Row> */}
-      {/* <Form.Group className="mb-3">
-        <Form.Check
-          required
-          label="Agree to terms and conditions"
-          feedback="You must agree before submitting."
-          feedbackType="invalid"
-        />
-      </Form.Group> */}
       <Button type="submit">Submit form</Button>
     </Form>
   );
