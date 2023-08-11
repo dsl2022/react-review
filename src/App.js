5361ffc890a327ed7bf787b2f4fa95a045c1aa62
dsl2022 <2015rpro@gmail.com>
2023-08-10 21:09:15 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import SubmitForm from './components/SubmitForm';
 import './App.css';
-
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
+  const [firstName, setFirstName]=useState("")
+  const [lastName, setLastName]=useState("")
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SubmitForm setLastName={setLastName} setFirstName={setFirstName} />
+      <span>{firstName}</span>
+      <br/>
+      <span>{lastName}</span>
+      {/* <ul>
+        <li>create a form,submit,search</li>
+        <li></li>
+      </ul> */}
     </div>
   );
 }
